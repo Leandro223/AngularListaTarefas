d06cad2ac0b116f744fc63cfc483cb747cc8a083
Baracho <baracholss@gmail.com>
2022-11-14 14:08:47 -0300

    metodo finDall lista todos implementado

--- src/app/components/read-all/read-all.component.ts
+++ src/app/components/read-all/read-all.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Todo } from 'src/app/models/todo';
+import { TodoService } from 'src/app/services/todo.service';
 
 @Component({
   selector: 'app-read-all',
@@ -12,9 +13,23 @@ export class ReadAllComponent implements OnInit {
   list: Todo[] = [];
   listFinished: Todo[] = [];
 
-  constructor() { }
+  constructor(private service: TodoService) { }
 
   ngOnInit(): void {
   }
+
+  findAll(): void {
+    this.service.findAll().subscribe((resposta) => {
+      resposta.forEach(todo => {
+        if (todo.finalizado){
+          this.listFinished.push(todo);
+        }else{
+          this.list.push(todo);
+        }
+      })
+      this.closed = this.listFinished.length;
+  })
+
+}
 
 }
