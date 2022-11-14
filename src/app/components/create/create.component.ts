3afb53a45ac6c00fa49e9c17a31e3f138e66bbc2
Baracho <baracholss@gmail.com>
2022-11-14 17:32:51 -0300

    metodo create e formatação de data criado

--- src/app/components/create/create.component.ts
+++ src/app/components/create/create.component.ts
@@ -1,28 +1,46 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { Todo } from 'src/app/models/todo';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { Todo } from "src/app/models/todo";
+import { TodoService } from "src/app/services/todo.service";
 
 @Component({
-  selector: 'app-create',
-  templateUrl: './create.component.html',
-  styleUrls: ['./create.component.css']
+  selector: "app-create",
+  templateUrl: "./create.component.html",
+  styleUrls: ["./create.component.css"],
 })
 export class CreateComponent implements OnInit {
   todo: Todo = {
-    titulo: '',
-    descricao: '',
+    titulo: "",
+    descricao: "",
     dataParaFinalizar: new Date(),
-    finalizado: false
+    finalizado: false,
+  };
+
+  constructor(private router: Router, private service: TodoService) {}
+
+  ngOnInit(): void {}
+
+  create(): void {
+    this.formataData();
+    this.service.create(this.todo).subscribe(
+      (resposta) => {
+        this.service.message("TO-DO Criado com sucesso!");
+        this.router.navigate([""]);
+      },
+      (err) => {
+        this.service.message("Falha ao criar TO-DO");
+      }
+    );
   }
-
-  constructor(private router: Router) { }
-
-  ngOnInit(): void {
-  }
-
 
   cancel(): void {
-    this.router.navigate([''])
+    this.router.navigate([""]);
   }
 
+  formataData(): void {
+    let data = new Date(this.todo.dataParaFinalizar);
+    this.todo.dataParaFinalizar = `${data.getDate()}/${
+      data.getMonth() + 1
+    }/${data.getFullYear()}`;
+  }
 }
